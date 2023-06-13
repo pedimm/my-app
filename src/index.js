6a65256194447cf23069766797c65de469a8db74
pedimm <pedroscabral04@gmail.com>
2023-06-13 15:16:52 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-import App from "./App";
+import React from 'react';
+import './index.css';
+import App from './App';
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
