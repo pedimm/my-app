425b6f7ec5a96f899e500a047bfe3951db5f9180
pedimm <pedroscabral04@gmail.com>
2023-06-12 23:39:46 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import './App.css';
-//import swapi;;
 
 export default function App() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
-  // useEffect(() => {
-  //   fetch('https://swapi.dev/api/people/1')
-  //     .then((response) => response.json())
-  //     .then((data) => console.log(data));
-  // });
-
-  if (email === "admin@gmail.com" && password === "admin") {
-
-  }
 
   const submit = (e) => {
     e.preventDefault();
@@ -34,7 +23,10 @@ export default function App() {
 
   return (
     <div id="root">
-      <form onSubmit={submit}>
+      <form id="form" onSubmit={submit}>
+        <label className="block text-sm font-medium leading-6 text-gray-900">
+          E-mail</label>
+          
         <input
           type="email"
           id="email"
@@ -44,11 +36,13 @@ export default function App() {
           onChange={(e) => setEmail(e.target.value)}
         ></input>
 
+        <label class="block text-sm font-medium leading-6 text-gray-900">Senha</label>
         <input
           type="password"
           id="password"
           name="password"
           placeholder="Senha"
+          className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         ></input>
